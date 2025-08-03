b61fe7c00d08bd6e7984b7c3528706ba34082339
spyros1973 <spyros1973@gmail.com>
2025-08-03 17:12:10 +0300

    Added plain menu screen

---
+++ settings.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.sound=true
+--M.scanlines=false
+--M.volume=0.5
+
+M.load=function()
+	local filename = sys.get_save_file("sys_save_load", "settings")
+	print("settings: "..filename)
+	if not sys.exists(filename) then return	end
+	local data = sys.load(filename) 
+	M.sound=data.sound or false
+	--M.scanlines=data.scanlines or false
+	--M.volume=data.volume or 0.5
+	return data
+end
+
+M.save = function()
+	local filename = sys.get_save_file("sys_save_load", "settings")
+	sys.save(filename, { sound = M.sound }) --, scanlines = M.scanlines, volume = M.volume })  
+end
+
+return M
