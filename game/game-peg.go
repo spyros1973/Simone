a663eecd92dd40b8f334ad8fb3336023c2ba7cc3
Spyros Paraschis <sparaschis@hotmail.com>
2025-08-22 08:52:33 +0300

    Graphics updated, added basic game over screen, life loss animation, remaining pegs scrolling message, wrong/right answer animation

--- game/game-peg.go
+++ game/game-peg.go
@@ -5,7 +5,7 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"Match3Clover\"\n"
+  data: "default_animation: \"peg-yellow\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
