ac97f777674fa4ed39c4bf7724383c8cf8fef405
spyros1973 <spyros1973@gmail.com>
2025-10-29 18:15:43 +0200

    Color overlay, selectable sounds, hit target bug fix
    Added color overlay to pegs, selectable sounds in settings, and fixed the peg hit target bug

--- settings.lua
+++ settings.lua
@@ -3,6 +3,7 @@ local M = {}
 M.sound=true
 --M.scanlines=false
 M.volume=0.5
+M.sound_theme="fx" -- notes|fx
 
 M.load=function()
 	local filename = sys.get_save_file("sys_save_load", "settings")
@@ -12,12 +13,13 @@ M.load=function()
 	M.sound=data.sound or false
 	--M.scanlines=data.scanlines or false
 	M.volume=data.volume or 0.5
+	M.sound_theme=data.sound_theme or "notes"
 	return data
 end
 
 M.save = function()
 	local filename = sys.get_save_file("sys_save_load", "settings")
-	sys.save(filename, { sound = M.sound, volume = M.volume }) --, scanlines = M.scanlines, volume = M.volume })  
+	sys.save(filename, { sound = M.sound, volume = M.volume, sound_theme=M.sound_theme}) --, scanlines = M.scanlines, volume = M.volume })  
 end
 
 return M
